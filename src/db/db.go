b91b699378e6ea4f69e804efa90375056f4c3ae8
pbentes <pedrogomes2000.3@gmail.com>
2024-02-03 02:18:34 +0000

    Added a basic SQLite3 setup with SQLx

--- src/db/db.go
+++ src/db/db.go
@@ -1,1 +1,26 @@
 package db
+
+import (
+	"github.com/jmoiron/sqlx"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var Database *sqlx.DB
+
+func InitDB() {
+	db, err := sqlx.Open("sqlite3", "file:application.db?cache=shared")
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	db.SetMaxOpenConns(1)
+	Database = db
+}
+
+func Cleanup() {
+	Database.Close()
+}
--- src/main.go
+++ src/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/pbentes/Chime/src/db"
 	"github.com/pbentes/Chime/src/router"
 )
 
 func main() {
+	db.InitDB()
+	defer db.Cleanup()
+
 	e := echo.New()
 
 	e.Static("/", "/assets")
