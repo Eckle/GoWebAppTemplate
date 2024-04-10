9cefd54ef7e6e1a3cd6f605d7b7ec05eb3a2bfd5
pbentes <pedrogomes2000.3@gmail.com>
2024-04-11 00:14:56 +0100

    Added a middleware to serve files and the index on the same root

--- src/router/router.go
+++ src/router/router.go
@@ -2,20 +2,34 @@ package router
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/a-h/templ"
 	"github.com/pbentes/80_20/src/templates/components"
 	"github.com/pbentes/80_20/src/templates/pages"
 )
 
+var dir string = "assets"
+var fs http.Handler = http.FileServer(http.Dir(dir))
+
 func SetupRouter() *http.ServeMux {
 	router := http.NewServeMux()
 
-	fs := http.FileServer(http.Dir("assets"))
-	router.Handle("GET /assets/", http.StripPrefix("/assets/", fs))
+	router.Handle("GET /", ServeFiles(templ.Handler(components.Layout(pages.Index))))
 
-	router.HandleFunc("GET /", templ.Handler(components.Layout(pages.Index)).ServeHTTP)
 	router.HandleFunc("GET /index", templ.Handler(pages.Index()).ServeHTTP)
 	router.HandleFunc("GET /content", templ.Handler(pages.Content()).ServeHTTP)
 	return router
+}
+
+func ServeFiles(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		path := dir + r.URL.Path
+		_, err := os.Stat(path)
+		if os.IsNotExist(err) || r.URL.Path == "/" {
+			next.ServeHTTP(w, r)
+		} else {
+			fs.ServeHTTP(w, r)
+		}
+	})
 }
