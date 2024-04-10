c6518efa99f9b4ab3da354a3f9a9ad3a291119b5
pbentes <pedrogomes2000.3@gmail.com>
2024-04-11 00:30:25 +0100

    Added the serve files middleware into its own middleware package

--- src/router/router.go
+++ src/router/router.go
@@ -2,34 +2,19 @@ package router
 
 import (
 	"net/http"
-	"os"
 
 	"github.com/a-h/templ"
-	"github.com/pbentes/80_20/src/templates/components"
-	"github.com/pbentes/80_20/src/templates/pages"
+	"github.com/pbentes/80_20/src/middleware"
+	"github.com/pbentes/80_20/src/views/components"
+	"github.com/pbentes/80_20/src/views/fragments"
 )
-
-var dir string = "assets"
-var fs http.Handler = http.FileServer(http.Dir(dir))
 
 func SetupRouter() *http.ServeMux {
 	router := http.NewServeMux()
 
-	router.Handle("GET /", ServeFiles(templ.Handler(components.Layout(pages.Index))))
+	router.Handle("GET /", middleware.ServeFiles(templ.Handler(components.Layout(fragments.Index))))
 
-	router.HandleFunc("GET /index", templ.Handler(pages.Index()).ServeHTTP)
-	router.HandleFunc("GET /content", templ.Handler(pages.Content()).ServeHTTP)
+	router.HandleFunc("GET /index", templ.Handler(fragments.Index()).ServeHTTP)
+	router.HandleFunc("GET /content", templ.Handler(fragments.Content()).ServeHTTP)
 	return router
-}
-
-func ServeFiles(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		path := dir + r.URL.Path
-		_, err := os.Stat(path)
-		if os.IsNotExist(err) || r.URL.Path == "/" {
-			next.ServeHTTP(w, r)
-		} else {
-			fs.ServeHTTP(w, r)
-		}
-	})
 }
